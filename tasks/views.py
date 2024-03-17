f8fefa4269b6636ddb7307eaaea20e9462059c28
Artem Milchenko <pl301078map2@gmail.com>
2024-03-17 18:02:38 +0200

    Created CRUD for Task model

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic, View
 
-# Create your views here.
+from tasks.forms import TaskCreateForm, TaskContentSearchForm
+from tasks.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    template_name = "tasks/index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+
+        content = self.request.GET.get("content", "")
+
+        context["search_form"] = TaskContentSearchForm(initial={"content": content})
+        return context
+
+    def get_queryset(self):
+        form = TaskContentSearchForm(self.request.GET)
+        if form.is_valid():
+            return (
+                super()
+                .get_queryset()
+                .filter(content__icontains=form.cleaned_data["content"])
+            )
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy("todo-app:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy("todo-app:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo-app:index")
+
+
